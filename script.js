46e8239c2538e110897aed1a5e04df2144672d8b
Amal <amalabdurahmonov427@gmail.com>
2023-03-18 11:20:40 +0300

    second

--- script.js
+++ script.js
@@ -1,130 +1,131 @@
-for(let i = 0; i < 20; i++){
-    for(let k = 20; k > i; k--) {
-        document.write('* ')
+let hotel = [
+    [
+      {
+        room: 1,
+        price: 300,
+        brand: "Luxe",
+        children: false,
+      },
+      {
+        room: 2,
+        price: 4000,
+        brand: "Deluxe",
+        children: true,
+      },
+      {
+        room: 3,
+        price: 1000,
+        brand: "Deluxe",
+        children: false,
+      },
+      {
+        room: 4,
+        price: 900,
+        brand: "Luxe",
+        children: true,
+      },
+    ],
+    [
+      {
+        room: 5,
+        price: 700,
+        brand: "Luxe",
+        children: true,
+      },
+      {
+        room: 6,
+        price: 79000,
+        brand: "President",
+        children: true,
+      },
+      {
+        room: 7,
+        price: 600,
+        brand: "Luxe",
+        children: false,
+      },
+      {
+        room: 8,
+        price: 950,
+        brand: "Luxe",
+        children: true,
+      },
+    ],
+    [
+      {
+        room: 9,
+        price: 1900,
+        brand: "Deluxe",
+        children: false,
+      },
+      {
+        room: 10,
+        price: 5000,
+        brand: "Deluxe",
+        children: true,
+      },
+      {
+        room: 11,
+        price: 1000,
+        brand: "Deluxe",
+        children: false,
+      },
+      {
+        room: 12,
+        price: 250,
+        brand: "Luxe",
+        children: false,
+      },
+    ],
+    [
+      {
+        room: 13,
+        price: 10000,
+        brand: "President",
+        children: true,
+      },
+      {
+        room: 14,
+        price: 8000,
+        brand: "President",
+        children: false,
+      },
+    ],
+  ];
+
+  
+  
+  let rooms = {
+    all_roms: [],
+    rooms: 0,
+  };
+
+
+
+  let brands = {
+    President: [],
+    Deluxe: [],
+    Luxe: [],
+  };
+  
+  let room_level = {
+    perviy: 0,
+    vtoroy: 0,
+    tretiy: 0,
+    chetvertiy: 0,
+  };
+
+  let totalPrice = hotel.reduce((a, b) => {
+    return a + b[1].price
+   }, 0)
+
+  for(let item of hotel) {
+    for(let categ of item){
+      hotel.eting = true
+        if(categ.brand == Object.keys(brands)) {
+            brands.push(categ.brand)
+            console.log('yes');
+        } 
     }
-    document.write('<br>')
 }
-
-
-let arr = [
-    {
-        id: Math.random(),
-        name: 'Timur',
-        info: {
-            school: '235',
-            faculity: 'SMM'
-        },
-    },
-    {
-        id: Math.random(),
-        name: 'Imran',
-        info: {
-            school: 'ne izvestno',
-            faculity: 'programming'
-        },
-    },
-    {
-        id: Math.random(),
-        name: 'Aminjon',
-        info: {
-            school: '444',
-            faculity: 'Dizayn'
-        },
-    },
-    {
-        id: Math.random(),
-        name: 'Maxmud',
-        info: {
-            school: '777',
-            faculity: '3dsmax'
-        },
-    },
-    {
-        id: Math.random(),
-        name: 'Muxammad',
-        info: {
-            school: '5555',
-            faculity: 'Backend'
-        },
-    },
-    {
-        id: Math.random(),
-        name: 'Timur',
-        info: {
-            school: '235',
-            faculity: 'SMM'
-        },
-    },
-    {
-        id: Math.random(),
-        name: 'Imran',
-        info: {
-            school: 'ne izvestno',
-            faculity: 'programming'
-        },
-    },
-    {
-        id: Math.random(),
-        name: 'Aminjon',
-        info: {
-            school: '444',
-            faculity: 'Dizayn'
-        },
-    },
-    {
-        id: Math.random(),
-        name: 'Maxmud',
-        info: {
-            school: '777',
-            faculity: '3dsmax'
-        },
-    },
-    {
-        id: Math.random(),
-        name: 'Maxmud',
-        info: {
-            school: '777',
-            faculity: '3dsmax'
-        },
-    },
-    {
-        id: Math.random(),
-        name: 'Muxammad',
-        info: {
-            school: '5555',
-            faculity: 'Backend'
-        },
-    },
-]
-
-let categories = [
-    {
-        course: ' SMM',
-        count: 0
-    },
-    {
-        course: 'PROGRAMMING',
-        count: 0
-    },
-    {
-        course: '   3DSMAX',
-        count: 0
-    },
-    {
-        course: ' DIZAYN',
-        count: 0
-    },
-    {       
-        course: '   BACKEND',
-        count: 0
-    },
-]
-
-for(let item of arr){
-    for(let categ of categories) {
-    if(item.info.faculity == categ.course){
-        categ.count++
-    } 
-    }
-}
-console.log(categories);
+  console.log(brands);
