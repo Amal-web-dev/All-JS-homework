18c47dd82b8db50e474bb0d4ceeecd7f01e50454
Amal <amalabdurahmonov427@gmail.com>
2023-03-25 11:04:51 +0300

    homework3

--- script.js
+++ script.js
@@ -1,131 +1,31 @@
-let hotel = [
-    [
-      {
-        room: 1,
-        price: 300,
-        brand: "Luxe",
-        children: false,
-      },
-      {
-        room: 2,
-        price: 4000,
-        brand: "Deluxe",
-        children: true,
-      },
-      {
-        room: 3,
-        price: 1000,
-        brand: "Deluxe",
-        children: false,
-      },
-      {
-        room: 4,
-        price: 900,
-        brand: "Luxe",
-        children: true,
-      },
-    ],
-    [
-      {
-        room: 5,
-        price: 700,
-        brand: "Luxe",
-        children: true,
-      },
-      {
-        room: 6,
-        price: 79000,
-        brand: "President",
-        children: true,
-      },
-      {
-        room: 7,
-        price: 600,
-        brand: "Luxe",
-        children: false,
-      },
-      {
-        room: 8,
-        price: 950,
-        brand: "Luxe",
-        children: true,
-      },
-    ],
-    [
-      {
-        room: 9,
-        price: 1900,
-        brand: "Deluxe",
-        children: false,
-      },
-      {
-        room: 10,
-        price: 5000,
-        brand: "Deluxe",
-        children: true,
-      },
-      {
-        room: 11,
-        price: 1000,
-        brand: "Deluxe",
-        children: false,
-      },
-      {
-        room: 12,
-        price: 250,
-        brand: "Luxe",
-        children: false,
-      },
-    ],
-    [
-      {
-        room: 13,
-        price: 10000,
-        brand: "President",
-        children: true,
-      },
-      {
-        room: 14,
-        price: 8000,
-        brand: "President",
-        children: false,
-      },
-    ],
-  ];
+function showMinNumber(n1) {
+    let num = n1
+    let min = num[0]
 
-  
-  
-  let rooms = {
-    all_roms: [],
-    rooms: 0,
-  };
-
-
-
-  let brands = {
-    President: [],
-    Deluxe: [],
-    Luxe: [],
-  };
-  
-  let room_level = {
-    perviy: 0,
-    vtoroy: 0,
-    tretiy: 0,
-    chetvertiy: 0,
-  };
-
-  let totalPrice = hotel.reduce((a, b) => {
-    return a + b[1].price
-   }, 0)
-
-  for(let item of hotel) {
-    for(let categ of item){
-      hotel.eting = true
-        if(categ.brand == Object.keys(brands)) {
-            brands.push(categ.brand)
-            console.log('yes');
-        } 
-    }
+    for (let item of num) {
+        if(item < min) {
+          min = item; 
+        }
+      }
+ console.log(min);
 }
-  console.log(brands);
+  
+ showMinNumber([1,-1,2])
+
+
+function sort(nums) {
+    let numbers = nums.length;
+    for (let i = 0; i < numbers - 1; i++) {
+      for (let k = 0; k < numbers - 1 - i; k++) {
+        if (nums[k + 1] < nums[k]) {
+          let t = nums[k + 1];
+          nums[k + 1] = nums[k];
+          nums[k] = t;
+        }
+      }
+    }
+    console.log(nums.at(-1));
+    return nums; 
+  }
+
+  sort([4,1,7,-8,10])
